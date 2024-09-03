73b30e954500eb5d68f853fa72568f87e0caf3ea
Mulde41 <145011065+Mulde41@users.noreply.github.com>
2024-09-03 12:32:01 +0200

    semifinished

---
+++ Project/Program.cs
@@ -0,0 +1,25 @@
+using System.Net.Mime;
+using System.Text;
+
+namespace Project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+
+
+            app.MapControllerRoute(name: "default",
+                pattern: "{controller=Home}/{action=index}/{id?}"
+                );
+
+
+
+            app.Run();
+        }
+    }
+}
