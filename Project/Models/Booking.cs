3a3ed4e93e1f3b995cfc94b38b52ea442d49072d
Jonathan Lipinski Rasmussen <jonathanlipinskirasmussen@gmail.com>
2024-09-23 18:23:46 +0200

    Login

--- Project/Models/Booking.cs
+++ Project/Models/Booking.cs
@@ -3,8 +3,7 @@
     public class Booking
     {
         public int BookingId { get; set; }
-        public User? User { get; set; }
-        public int? UserId { get; set; }
+        public AppUser? User { get; set; }
         public List<Surfboard>? Surfboards { get; set; }
 
         
