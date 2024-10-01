f938656e924b0383a5d14272da0dc35f4ec2495c
RasmusRosengaard <rrni58420@edu.ucl.dk>
2024-10-01 09:54:15 +0200

    added basic classses

---
+++ Models/Equipment.cs
@@ -0,0 +1,9 @@
+﻿namespace Surf_s_up.Models
+{
+    public class Equipment
+    {
+        public int EquipmentId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ Models/SurfboardType.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace Surf_s_up.Models
+{
+    enum Types {
+        [Description("The Mini Log")]
+        TheMinilog,
+
+        [Description("The Wide Glider")]
+        TheWideGlider,
+
+        [Description("The Golden Ratio")]
+        TheGoldenRatio,
+
+        [Description("The Emerald Glider")]
+        TheEmeraldGlider,
+
+        [Description("The Bomb")]
+        TheBomb,
+
+        [Description("The Walden Magic")]
+        WaldenMagic,
+
+        [Description("Naish One")]
+        NaishOne,
+
+        [Description("Six Tourer")]
+        SixTourer,
+
+        [Description("Naish Maliko")]
+        NaishMaliko 
+    }
+}
